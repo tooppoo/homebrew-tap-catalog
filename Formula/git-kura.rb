50a325de453de312591e332dd986306be75cedc8
philomagi <tooppoo@users.noreply.github.com>
2026-07-01 21:00:31 +0900

    del prefix

--- Formula/git-kura.rb
+++ Formula/git-kura.rb
@@ -9,12 +9,12 @@ class GitKura < Formula
   on_macos do
     on_arm do
       url "https://github.com/tooppoo/git-kura/releases/download/v#{version}/git-kura_v#{version}_Darwin_arm64.tar.gz"
-      sha256 "sha256:df5f726ca8fd8d42c594543ef60edb3ae2965fb0bd741b35dfa01c3bf61d974a"
+      sha256 "df5f726ca8fd8d42c594543ef60edb3ae2965fb0bd741b35dfa01c3bf61d974a"
     end
 
     on_intel do
       url "https://github.com/tooppoo/git-kura/releases/download/v#{version}/git-kura_v#{version}_Darwin_x86_64.tar.gz"
-      sha256 "sha256:21acd2c3f729831f5ea18fdca1b9f725beacc94df896ebfb9b7af81ec8e6fa7f"
+      sha256 "21acd2c3f729831f5ea18fdca1b9f725beacc94df896ebfb9b7af81ec8e6fa7f"
     end
   end
 
